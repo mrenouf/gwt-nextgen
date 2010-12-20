b7797d2fda28805c3087621f27d592da49bc9ccf
Mark Renouf <mark.renouf@gmail.com>
2010-12-20 09:16:53 -0500

    Whitespace and formatting cleanup

--- src/main/java/com/google/gwt/websocket/client/WebSocketImpl.java
+++ src/main/java/com/google/gwt/websocket/client/WebSocketImpl.java
@@ -1,48 +1,45 @@
 package com.google.gwt.websocket.client;
 
 import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.core.client.UnsafeNativeLong;
 
 class WebSocketImpl extends JavaScriptObject {
 
-	protected WebSocketImpl() {
-	}
+  protected WebSocketImpl() {}
 
-	public native final int getReadyState() /*-{
-		return this.readyState;
-	}-*/;
+  public native final int getReadyState() /*-{
+    return this.readyState;
+  }-*/;
 
-	public static final native WebSocketImpl create(WebSocket peer, String url) /*-{
-		var socket = new WebSocket(url);
-		socket.onopen = function(e) {
-			peer.@com.google.gwt.websocket.client.WebSocket::handleOpenEvent(Lcom/google/gwt/dom/client/NativeEvent;)(e);
-		}
-		socket.onmessage = function(e) {
-			peer.@com.google.gwt.websocket.client.WebSocket::handleMessageEvent(Lcom/google/gwt/dom/client/NativeEvent;)(e);
-		}
-		socket.onerror = function(e) {
-			peer.@com.google.gwt.websocket.client.WebSocket::handleErrorEvent(Lcom/google/gwt/dom/client/NativeEvent;)(e);
-		}
-		socket.onclose = function(e) {
-			peer.@com.google.gwt.websocket.client.WebSocket::handleCloseEvent(Lcom/google/gwt/dom/client/NativeEvent;)(e);
-		}
-		return socket;
-	}-*/;
+  public static final native WebSocketImpl create(WebSocket peer, String url) /*-{
+    var socket = new WebSocket(url);
+    socket.onopen = function(e) {
+      peer.@com.google.gwt.websocket.client.WebSocket::handleOpenEvent(Lcom/google/gwt/dom/client/NativeEvent;)(e);
+    }
+    socket.onmessage = function(e) {
+      peer.@com.google.gwt.websocket.client.WebSocket::handleMessageEvent(Lcom/google/gwt/dom/client/NativeEvent;)(e);
+    }
+    socket.onerror = function(e) {
+      peer.@com.google.gwt.websocket.client.WebSocket::handleErrorEvent(Lcom/google/gwt/dom/client/NativeEvent;)(e);
+    }
+    socket.onclose = function(e) {
+      peer.@com.google.gwt.websocket.client.WebSocket::handleCloseEvent(Lcom/google/gwt/dom/client/NativeEvent;)(e);
+    }
+    return socket;
+  }-*/;
 
-	@UnsafeNativeLong
-	public native final long getBufferedAmount() /*-{
-		return this.bufferedAmount;
-	}-*/;
+  public native final double getBufferedAmount() /*-{
+    return this.bufferedAmount;
+  }-*/;
 
-	public native final String getProtocol() /*-{
-		return this.protocol;
-	}-*/;
+  public native final String getProtocol() /*-{
+    return this.protocol;
+  }-*/;
 
-	public native final void send(String message) /*-{
-		this.send(message);
-	}-*/;
+  public native final void send(String message) /*-{
+    this.send(message);
+  }-*/;
 
-	public native final void close() /*-{
-		this.close();
-	}-*/;
+  public native final void close() /*-{
+    this.close();
+  }-*/;
 }
